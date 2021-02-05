53664d823bec4b8dfe1407592f9b8bfd97c555b0
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-02-05 13:37:04 +0700

    Rebase

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+const calcDuration = () => {
+  var startDate = new Date(2017, 02, 26, 0, 0, 0);
+  var now = new Date();
+  var secondsDiff = Math.round((now - startDate) / (1000));
+  var minutesDiff = Math.round((now - startDate) / (1000*60));
+  var hoursDiff = Math.round((now - startDate) / (1000*60*60));
+  var daysDiff = Math.round((now - startDate) / (1000*60*60*24));
+  var nextAnniversary = (daysDiff - (daysDiff % 100) + 100);
+  var nextDate = new Date(startDate.getTime() + nextAnniversary*24*60*60*1000);
+  
+  document.getElementById("secondsDiff").innerHTML = secondsDiff;
+  document.getElementById("hoursDiff").innerHTML = hoursDiff;
+  document.getElementById("minutesDiff").innerHTML = minutesDiff;
+  document.getElementById("daysDiff").innerHTML = daysDiff;
+  document.getElementById("nextAnniversary").innerHTML = nextAnniversary;
+  document.getElementById("nextDate").innerHTML = nextDate.toDateString();
+}
+
+setInterval(calcDuration, 1000);
+
